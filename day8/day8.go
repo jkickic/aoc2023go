07459d7f9e83b7e4498d2fb761a981df26e71073
mzajac <pentacor.zajac@extaccount.com>
2024-08-10 12:01:56 +0200

    aoc2023 - day8 - solve part 2

--- day8/day8.go
+++ day8/day8.go
@@ -3,6 +3,7 @@ package day8
 import (
 	"aoc2023go/utils"
 	"regexp"
+	"strings"
 )
 
 type LR struct {
@@ -10,7 +11,7 @@ type LR struct {
 	right string
 }
 
-func Solve(file string) int {
+func SolvePart1(file string) int {
 	lines := utils.LoadFileLines(file)
 
 	directions := lines[0]
@@ -38,4 +39,64 @@ func Solve(file string) int {
 		counter++
 	}
 	return counter
+}
+
+func SolvePart2(file string) int {
+	lines := utils.LoadFileLines(file)
+
+	directions := lines[0]
+	directionsMap := make(map[string]LR)
+	var startingKeys []string
+
+	re := regexp.MustCompile("[^a-zA-Z0-9]+")
+	for i := 2; i < len(lines); i++ {
+		line := lines[i]
+		res := re.Split(line, -1)
+		directionsMap[res[0]] = LR{res[1], res[2]}
+		if strings.HasSuffix(res[0], "A") {
+			startingKeys = append(startingKeys, res[0])
+		}
+	}
+
+	results := []int{}
+
+	for _, key := range startingKeys {
+		counter := 0
+		for !strings.HasSuffix(key, "Z") {
+			directionIndex := counter % len(directions)
+			direction := string(directions[directionIndex])
+			lr := directionsMap[key]
+			if direction == "L" {
+				key = lr.left
+			} else {
+				key = lr.right
+			}
+			counter++
+		}
+		results = append(results, counter)
+	}
+
+	return lcmArray(results)
+}
+
+// COPILOT CHEATS:
+func gcd(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+func lcm(a, b int) int {
+	return (a * b) / gcd(a, b)
+}
+
+func lcmArray(arr []int) int {
+	result := arr[0]
+	for _, num := range arr[1:] {
+		result = lcm(result, num)
+	}
+	return result
 }
--- day8/day8_test.go
+++ day8/day8_test.go
@@ -3,15 +3,29 @@ package day8
 import "testing"
 
 func TestSample(t *testing.T) {
-	var count = Solve("test_input.txt")
+	var count = SolvePart1("test_input.txt")
 	if count != 6 {
 		t.Fatalf("Expected 6, but got %d", count)
 	}
 }
 
 func TestSolve(t *testing.T) {
-	var count = Solve("input.txt")
+	var count = SolvePart1("input.txt")
 	if count != 20221 {
 		t.Fatalf("Expected 20221, but got %d", count)
+	}
+}
+
+func TestSamplePart2(t *testing.T) {
+	var count = SolvePart2("test_input2.txt")
+	if count != 6 {
+		t.Fatalf("Expected 6, but got %d", count)
+	}
+}
+
+func TestSolvePart2(t *testing.T) {
+	var count = SolvePart2("input.txt")
+	if count != 14616363770447 {
+		t.Fatalf("Expected 14616363770447, but got %d", count)
 	}
 }
